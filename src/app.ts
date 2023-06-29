7dcd98b5ab6747dcdb9e937f186d36e407410f7f
lehinfo-felix <lehinfo.felix@gmail.com>
2023-06-29 01:49:43 -0300

    fix: simplify CORS rules

--- src/app.ts
+++ src/app.ts
@@ -13,27 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(helmet())
-app.use(
-  cors({
-    origin: '*',
-  }),
-)
-// app.use(
-//   (
-//     req: any,
-//     res: { header: (arg0: string, arg1: string) => void },
-//     next: () => void,
-//   ) => {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-//     res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//     )
-
-//     next()
-//   },
-// )
+app.use(cors({ origin: '*' }))
 app.use('/api/', router)
 
 app.listen(port, () => {
